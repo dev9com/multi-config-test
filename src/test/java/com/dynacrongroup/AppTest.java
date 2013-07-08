50cedbbe5fda742e802f306bb57767256633109d
Will Iverson <wiverson@gmail.com>
2013-07-08 11:14:49 -0700

    Basic test and logs

---
+++ src/test/java/com/dynacrongroup/AppTest.java
@@ -0,0 +1,38 @@
+package com.dynacrongroup;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    public AppTest() {
+        logger.info("AppTest Constructor");
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+        logger.info("AppTest.testApp called");
+    }
+
+    @Test
+    public void whichJdk() {
+        assertTrue(true);
+
+        logger.info("JDK being run is " + System.getProperty("java.version"));
+    }
+
+    @Test
+    public void whichOs() {
+        logger.info("OS being run is " + System.getProperty("os.version"));
+    }
+
+    @Test
+    public void sampleProperty() {
+        logger.info("sample.property is " + System.getProperty("sample.property"));
+    }
+}
